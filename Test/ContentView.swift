fb0793f43372073ef032da8643b2136ec8fa1f29
sungro <sungro2008@gmail.com>
2021-07-23 08:06:54 +0200

    hello kai

--- Test/ContentView.swift
+++ Test/ContentView.swift
@@ -8,9 +8,74 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var textString = "Click the Button"
+    @State private var value = ""
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+        Color(.systemTeal)
+            .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            VStack {
+                Text(textString)
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundColor(Color.white)
+                
+                Text(value)
+                
+                HStack {
+                    Button(action: {
+                        value = "Hello World!"
+                    }) {
+                        Text("show")
+                            .font(.body)
+                            .fontWeight(.bold)
+                            .foregroundColor(Color.yellow)
+                            .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                            .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color.gray/*@END_MENU_TOKEN@*/)
+                    }
+                    
+                    Button(action: {
+                        value = ""
+                    }) {
+                        Text("hide")
+                            .font(.body)
+                            .fontWeight(.bold)
+                            .foregroundColor(Color.yellow)
+                            .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                            .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color.gray/*@END_MENU_TOKEN@*/)
+                    }
+                }
+                
+                HStack {
+                    ExtractedView(btnName: "show")
+                    ExtractedView(btnName: "hide")
+                }
+                
+            }
+        }
+    }
+}
+
+struct ExtractedView: View {
+    
+    let btnName : String
+    
+    var body: some View {
+        Button(action : {
+            // Dont know how to change value from here.
+            // I have created some functions but its not working properly becouse it comes "self is immutable"
+        }) {
+            Text(btnName)
+                .font(.body)
+                .fontWeight(.bold)
+                .foregroundColor(Color.yellow)
+                .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color.gray/*@END_MENU_TOKEN@*/)
+        }
+        .padding(.all)
+        
     }
 }
 
@@ -19,3 +84,5 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
